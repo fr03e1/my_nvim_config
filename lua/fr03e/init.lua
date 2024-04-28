8b592787e40aaa5aeb4530a53e55eb0649eb0af3
fr03e1 <aleksejbocarov65@gmail.com>
2024-04-28 23:21:48 +0300

    add editor settings

--- lua/fr03e/init.lua
+++ lua/fr03e/init.lua
@@ -1,2 +1,5 @@
 require("fr03e.remap")
 require("fr03e.lazy_init")
+require("fr03e.set")
+
+
--- lua/fr03e/lazy/lsp/mason.lua
+++ lua/fr03e/lazy/lsp/mason.lua
@@ -44,7 +44,6 @@ return {
       ensure_installed = {
         "prettier", -- prettier formatter
         "stylua", -- lua formatter
-        "pylint",
         "eslint_d",
       },
     })
